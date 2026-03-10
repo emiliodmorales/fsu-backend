ea4c41c049223d915d28b279b58c36cb2cc4e188
Froggles <elmsterm@gmail.com>
2026-03-10 12:02:34 -0400

    add departments post route

--- api/departments.js
+++ api/departments.js
@@ -1,4 +1,11 @@
-import { getDepartmentById, getDepartments } from "#db/queries/departments";
+import {
+  createDepartment,
+  getDepartmentById,
+  getDepartments,
+} from "#db/queries/departments";
+import requireAdmin from "#middleware/requireAdmin";
+import requireBody from "#middleware/requireBody";
+
 import { Router } from "express";
 const router = Router();
 export default router;
@@ -7,6 +14,22 @@ router.get("/", async (req, res) => {
   const departments = await getDepartments();
   res.send(departments);
 });
+
+router.post(
+  "/",
+  requireBody(["name", "description", "images", "email", "phone"]),
+  async (req, res) => {
+    const { name, description, images, email, phone } = req.body;
+    const department = await createDepartment({
+      name,
+      description,
+      images,
+      email,
+      phone,
+    });
+    res.status(201).send(department);
+  },
+);
 
 router.param("id", async (req, res, next) => {
   const { id } = req.params;
