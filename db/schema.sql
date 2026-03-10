e0a0a2759dc5ddb6a12e7a4f85c28aee69fd8d20
Froggles <elmsterm@gmail.com>
2026-03-10 11:22:10 -0400

    define database schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS professors;
+
+DROP TABLE IF EXISTS departments;
+
+DROP TABLE IF EXISTS admins;
+
+CREATE TABLE
+  departments (
+    id serial PRIMARY KEY,
+    name text UNIQUE,
+    description text,
+    images text ARRAY,
+    email text,
+    phone text
+  );
+
+CREATE TABLE
+  professors (
+    id serial PRIMARY KEY,
+    name text UNIQUE,
+    bio text,
+    profile_image text,
+    email text,
+    phone text,
+    department bigint REFERENCES departments (id) ON DELETE CASCADE
+  );
+
+CREATE TABLE
+  admins (username text PRIMARY KEY, password text);
