3fcc4238d4929bcf2fb2760e252f6ec139fbe651
Froggles <elmsterm@gmail.com>
2026-03-10 11:43:11 -0400

    create get department details route

--- api/departments.js
+++ api/departments.js
@@ -1,4 +1,4 @@
-import { getDepartments } from "#db/queries/departments";
+import { getDepartmentById, getDepartments } from "#db/queries/departments";
 import { Router } from "express";
 const router = Router();
 export default router;
@@ -6,4 +6,18 @@ export default router;
 router.get("/", async (req, res) => {
   const departments = await getDepartments();
   res.send(departments);
+});
+
+router.param("id", async (req, res, next) => {
+  const { id } = req.params;
+
+  const department = await getDepartmentById(id);
+  if (!department) return res.status(400).send("Invalid department id.");
+
+  req.department = department;
+  next();
+});
+
+router.get("/:id", (req, res) => {
+  res.send(req.department);
 });
--- db/queries/departments.js
+++ db/queries/departments.js
@@ -41,3 +41,14 @@ export async function getDepartments() {
   const { rows: departments } = await db.query(sql);
   return departments;
 }
+
+export async function getDepartmentById(id) {
+  const sql = `
+    SELECT * FROM departments
+    WHERE id=$1
+  `;
+  const {
+    rows: [department],
+  } = await db.query(sql, [id]);
+  return department;
+}
