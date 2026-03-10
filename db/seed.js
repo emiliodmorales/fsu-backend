04d93d4a474d74532decd529b41f4a2e4d3f9b8a
Froggles <elmsterm@gmail.com>
2026-03-10 11:23:22 -0400

    seed the databse with dummy data

---
+++ db/seed.js
@@ -0,0 +1,32 @@
+import db from "#db/client";
+import { createDepartment } from "#db/queries/departments";
+import { createProfessor } from "#db/queries/professors";
+import { createAdmin } from "#db/queries/admins";
+
+await db.connect();
+await seed();
+await db.end();
+console.log("🌱 Database seeded.");
+
+async function seed() {
+  const math = await createDepartment({
+    name: "Math",
+    description: "We love numbers",
+    images: [
+      "https://img.freepik.com/free-photo/blackboard-inscribed-with-scientific-formulas-calculations_1150-19413.jpg?semt=ais_hybrid&w=740&q=80",
+    ],
+    email: "math@fsu.edu",
+    phone: "(161)654-5654",
+  });
+
+  const bill = await createProfessor({
+    name: "Bill",
+    bio: "I like math",
+    profileImage: "https://www.bard.edu/files/programs/image.php?id=3200091",
+    email: "bill@fsu.edu",
+    phone: "(243)198-1448",
+    department: math.id,
+  });
+
+  const dean = await createAdmin("dean", "password123");
+}
